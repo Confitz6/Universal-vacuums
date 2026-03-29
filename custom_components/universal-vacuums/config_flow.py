28bd97137bffe802a0a56734376c84913b0e6f5b
Confitz6 <120193620+Confitz6@users.noreply.github.com>
2026-03-28 23:43:51 -0400

    Add files via upload

---
+++ custom_components/universal-vacuums/config_flow.py
@@ -0,0 +1,16 @@
+from homeassistant import config_entries
+from .const import DOMAIN
+
+class UniversalVacuumsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Universal Vacuums."""
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
+
+        if user_input is not None:
+            return self.async_create_entry(title="Universal Vacuums", data={})
+
+        return self.async_show_form(step_id="user")
